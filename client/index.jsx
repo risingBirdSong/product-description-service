3926d910b2c89426771e5e999202c9cf9e8ad802
risingbirdsong <heregoingthere@outlook.com>
2020-05-09 17:55:08 -0700

    erased broken import

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDom from "react-dom";
 import App from "./App";
-import Landing from "./components/landing";
 
 ReactDom.render(<App />, document.getElementById("root"));
